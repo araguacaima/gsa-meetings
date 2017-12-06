3ac764dcfb1aed4f34f979a3cf108ea4900fe498
Alex <araguacaima@gmail.com>
2017-12-06 09:36:19 -0600

    Adding database.js

---
+++ config/database.js
@@ -0,0 +1,1 @@
+module.exports.url="mongodb://admin:niga032.@cluster0-shard-00-00-3c6kr.mongodb.net:27017,cluster0-shard-00-01-3c6kr.mongodb.net:27017,cluster0-shard-00-02-3c6kr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
